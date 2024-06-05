a7133c80f07426f3621f94671c5e608499fbe6ae
Suk Wah Ho <sukwah.ho@triosstudent.com>
2024-06-05 19:31:19 -0400

    v1

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom'; 
-import logo from './logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap'; 
 import './nav.css';
@@ -9,9 +8,6 @@ const CustomNavbar = ({ balance }) => {
     return ( 
         <Navbar bg="light" expand="lg" fixed="top"> 
             <div className="container" > 
-                <Navbar.Brand href="#">
-                    <img src={logo} className="App-logo" alt="HSBC Bank Canada"  />
-                </Navbar.Brand>
                 <ul className="nav justify-content-end">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
